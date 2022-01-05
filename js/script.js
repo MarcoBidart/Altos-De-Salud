1969536d11c2e88eeddb4c75b54ac0f15006f745
Marco <marcobidart7@gmail.com>
2022-01-05 15:02:54 -0300

    Rediseno de pagina e implementacion de pag prp

--- js/script.js
+++ js/script.js
@@ -1,16 +1,44 @@
-let entrada1 = prompt('Antes de comezar ingresa tu Nombre');
-let entrada2 = prompt("Ahora ingresa tu Apellido")
-let entrada3 = parseInt(prompt("Cual es tu edad" + " " + entrada1 + ' ' + entrada2 + "?"))
-let salida = entrada1 + ' ' + entrada2 + ' ' + 'Has ingresado Correctamente tus Datos';
-alert(salida);
 
-if (entrada3 < 13) {
-    alert("Lo sentimos mucho" + ' ' + entrada1 + ' ' + entrada2 + ' ' +
-        " no tienes la edad requerida para utilizar nuestros productos");
-} else if (entrada3 < 18) {
-    alert("Bienvenido" + ' ' + entrada1 + ' ' + entrada2 + ' ' +
-        "a la hora de reservar un turno debe demostrar su identidad mediante DNI e ir con un adulto al mismo debido a que tienes " + entrada3 + ' ' + "años");
-} else {
-    alert("Bienvenido" + ' ' + entrada1 + ' ' + entrada2 + ' ' +
-    "ya puede disfrutar de nuestras promociones exclusivas de la pagina");
-}
+// //       Promt de bienvenida LOGIN
+
+// let entrada1 = prompt('Antes de comezar ingresa tu Nombre');
+
+// // alert(SALIDA NOMBRE);
+// if (entrada1 === ""){
+//     alert("ERROR: Es necesario que ingreses tus datos");}
+
+// //       Promt de bienvenida LOGIN
+// let entrada2 = prompt("Ahora ingresa tu Apellido")
+
+
+// //  alert(SALIDA APELLIDO)
+
+// if (entrada2 === "") {
+//     alert("ERROR: Es necesario que ingreses tus datos");
+// } else {
+//     alert (entrada1 + " has ingresado correctamente tus datos");
+//  }
+
+// //       Promt de bienvenida LOGIN
+// let entrada3 = parseInt(prompt("Cual es tu edad" + " " + entrada1 + "?"))
+
+
+
+// //  alert(SALIDA EDAD)
+
+// if (entrada3 == '') {
+//     alert (entrada1 + " debes ingresar tu edad para continuar")
+// }
+
+
+// //  CONDICIONALES DEL LOGIN
+// if (entrada3 < 13) {
+//     alert("Lo sentimos mucho" + ' ' + entrada1 + ' ' +
+//         " no tienes la edad requerida para utilizar nuestros productos");
+// } else if (entrada3 < 18) {
+//     alert("Bienvenido" + ' ' + entrada1 + ' ' + entrada2 + ' ' +
+//         "a la hora de reservar un turno debe demostrar su identidad mediante DNI e ir con un adulto al mismo debido a que tienes " + entrada3 + ' ' + "años");
+// } else {
+//     alert("Bienvenido" + ' ' + entrada1 + ' ' + entrada2 + ' ' +
+//     "ya puede disfrutar de nuestras promociones exclusivas de la pagina");
+// }
